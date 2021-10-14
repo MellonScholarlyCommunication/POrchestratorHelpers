700ebee8c6d73da103c86beefad092f1b338b929
Patrick Hochstenbach <Patrick.Hochstenbach@UGent.be>
2021-10-14 11:34:22 +0200

    Adding a rule tester

---
+++ bin/test_rules.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+
+URL=$1
+RULES=$2
+
+if [ $# -ne 2 ]; then
+    echo "usage: $0 url-to-remote-notification rules"
+    exit 1
+fi
+
+shift
+
+TMPFILE=$(mktemp)
+
+solid_auth.pl curl -H "Accept: application/ld+json" ${URL} > ${TMPFILE}
+
+orchestrator/bin/orchestrator.sh ${TMPFILE} `pwd`/$@
+
+rm ${TMPFILE}
