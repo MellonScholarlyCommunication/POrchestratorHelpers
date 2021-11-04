1397de8a54c50939b3c871ecb7e2e5e3036056cd
Patrick Hochstenbach <Patrick.Hochstenbach@UGent.be>
2021-11-04 11:33:40 +0100

    Adding support for Mustach templating to find out the subject of a Frame

--- src/turtle_jsonld.ts
+++ src/turtle_jsonld.ts
@@ -1,24 +1,8 @@
 import fs from 'fs';
 import * as N3 from 'n3';
 import * as jsonld from 'jsonld';
-import { json } from 'stream-consumers';
 import { Command } from 'commander';
-
-// Copied from AlexeyMz/ramp-shapeds-perf src/jsonld.ts
-jsonld.registerRDFParser('text/turtle', input => {
-    return new Promise((resolve, reject) => {
-      const quads: N3.Quad[] = [];
-      new N3.Parser().parse(input, (error, quad, hash) => {
-        if (error) {
-          reject(error);
-        } else if (quad) {
-          quads.push(quad);
-        } else {
-          resolve(quads as jsonld.Quad[]);
-        }
-      });
-    });
-});
+import * as Mustache from 'mustache';
 
 const program  = new Command();
 
@@ -31,15 +15,21 @@ program.argument('<file>')
             });
        });
 
-program.parse();
+program.parse(process.argv);
 
 async function quadsToJson(inputFile: string, format: string, frameFile: string) {
     const data  = fs.readFileSync(inputFile,'utf-8');
    
-    const ld = await jsonld.fromRDF(data,{ format: format});
+    jsonld.registerRDFParser(format, input => N3Parser(input));
+
+    const ld = await jsonld.fromRDF(data,{format: format});
 
     if (typeof frameFile !== 'undefined') {
-        const frameJson = fs.readFileSync(frameFile, 'utf-8');
+        const subject   = await getSubject(data);
+        let frameJson = fs.readFileSync(frameFile, 'utf-8');
+
+        frameJson = Mustache.render(frameJson, { id: subject });
+
         const frameData = JSON.parse(frameJson);
         const json = await jsonld.frame(ld,frameData);        
         return JSON.stringify(json,null,2);
@@ -48,4 +38,39 @@ async function quadsToJson(inputFile: string, format: string, frameFile: string)
         const json = await jsonld.flatten(ld);
         return JSON.stringify(json,null,2);
     }
+}
+
+async function N3Parser(input: string) : Promise<N3.Quad[]> {
+    return new Promise((resolve, reject) => {
+      const quads: N3.Quad[] = [];
+      new N3.Parser().parse(input, (error, quad, _) => {
+        if (error) {
+          reject(error);
+        } else if (quad) {
+          quads.push(quad);
+        } else {
+          resolve(quads as jsonld.Quad[]);
+        }
+      });
+    });
+}
+
+async function getSubject(input: string) : Promise<string | null> {
+      const seen = new Set();
+   
+      const quads = await N3Parser(input);
+
+      quads.forEach( quad => {
+          seen.add( quad.subject.value );
+      });
+
+      quads.forEach( quad => {
+          seen.delete( quad.object.value );
+      });
+
+      if (seen.size == 1) {
+          return seen.values().next().value;
+      }
+
+      return null;
 }
